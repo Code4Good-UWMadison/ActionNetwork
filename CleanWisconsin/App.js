885d443d917ebf017153551a440fdbe43a5db7dd
Kyle Verheyen <kyleverheyen@Kyles-MacBook-Pro.local>
2021-08-10 18:51:56 -0500

    Added functionality to send email for feature request

--- CleanWisconsin/App.js
+++ CleanWisconsin/App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Button, View } from 'react-native'
+import email from 'react-native-email'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button title="Send Mail" onPress={this.handleEmail} />
+            </View>
+        )
+    }
+
+    handleEmail = () => {
+        //TODO: Change to clean wiscos email for prod
+        const to = ['kyleverheyen@gmail.com']
+        email(to, {
+            subject: 'Feature Recommendation',
+            body: 'Enter feature here'
+        }).catch(console.error)
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
