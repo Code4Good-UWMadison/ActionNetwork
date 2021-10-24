f52a1db3a3ed0035d50023e9a762df7a35f0aa76
Charmaine Seah <80232795+charmainessseah@users.noreply.github.com>
2021-10-24 14:50:32 -0500

    added functionality to request news articles

--- data.js
+++ data.js
@@ -3,13 +3,13 @@ const cheerio = require('cheerio');
 let url = "https://www.cleanwisconsin.org/news/press-releases/";
 
 async function configureBrowser() {
-    let browser = await puppeteer.launch();
+    let browser = await puppeteer.launch({headless: false});
     let page = await browser.newPage();
     await page.goto(url);
     return page;
 }
 
-async function getContentNewsFeed(page) {
+async function getPreviewNewsFeed(page) {
     await page.reload();
     let data = await page.evaluate(() => document.body.innerHTML);
     const $ = cheerio.load(data);
@@ -35,15 +35,101 @@ async function getContentNewsFeed(page) {
         };
         return data;
     }
-    
-    const jsonData = JSON.stringify(mapData(titles, articleBody));
+
+    const previewData = mapData(titles, articleBody);
+   
+    const jsonData = JSON.stringify(previewData);
     console.log(jsonData);
 }
 
-async function start() {
+async function getPreviewData() {
     let page = await configureBrowser();
-    await getContentNewsFeed(page);
+    await getPreviewNewsFeed(page);
+    await browser.close
 }
 
-start();
+
+//-------------------------------------------------------------------------------
+
+async function getArticleContent() {
+
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    // await page.reload();
+    
+    const links = await page.evaluate(
+        () => Array.from(
+          document.querySelectorAll('.entry-title a'),
+          a => a.getAttribute('href')
+        )
+      );
+    
+    
+    const mapData = (titles, bodies) => {
+        const data = [];
+        for (let i = 0; i < titles.length; i++) {
+            data.push({
+                title: titles[i],
+                body: bodies[i],
+            });
+        }
+        return data;
+    }
+    // console.log(links[0]);
+    // await page.goto(links[0]);
+    // await page.reload();
+    // let data = await page.evaluate(() => document.body.innerHTML);
+    // const $ = cheerio.load(data);
+
+    // // how to grab title and article body from each link? 
+    // let title = $('.entry-title').text();
+    // console.log(title);
+    // let body = $('.et_pb_text_inner').text();
+    // console.log(body);
+        let titles = [];
+        let bodies = [];
+    for (let i = 0; i < links.length; i++) {
+        let link = links[i];
+        await page.goto(link);
+        await page.reload();
+        let data = await page.evaluate(() => document.body.innerHTML);
+        const $ = cheerio.load(data);
+
+        let title = $('.entry-title').text();
+        titles.push(title);
+        let body = $('.et_pb_text_inner').text();
+        bodies.push(body);
+    }
+
+    const data = mapData(titles, bodies);
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+        
+
+    return jsonData;
+
+    //browser.close();
+    
+      
+    
+    // let list = [...nodeList];
+    // list.forEach(div => {titles.push(div.querySelector('a[href]').text)});
+    
+
+    //await page.click("article a")
+    //console.log(articleBodies.length);
+}
+
+async function getArticles() {
+    //let page = await configureBrowser();
+    await getArticleContent();
+}
+
+//--------------------------------------------------------------------------------
+
+//getPreviewData();
+getArticles();
+
 
--- server/index.js
+++ server/index.js
@@ -2,8 +2,67 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("This api will be used for scraping data from Clean Wisconsin's website");
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+let url = "https://www.cleanwisconsin.org/news/press-releases/";
+//let jsonData = getArticles(); 
+
+async function getArticles() {
+
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    // await page.reload();
+    
+    const links = await page.evaluate(
+        () => Array.from(
+          document.querySelectorAll('.entry-title a'),
+          a => a.getAttribute('href')
+        )
+      );
+    
+    
+    const mapData = (titles, bodies) => {
+        const data = [];
+        for (let i = 0; i < titles.length; i++) {
+            data.push({
+                title: titles[i],
+                body: bodies[i],
+            });
+        }
+        return data;
+    }
+
+    let titles = [];
+    let bodies = [];
+    for (let i = 0; i < links.length; i++) {
+        let link = links[i];
+        await page.goto(link);
+        await page.reload();
+        let data = await page.evaluate(() => document.body.innerHTML);
+        const $ = cheerio.load(data);
+
+        let title = $('.entry-title').text();
+        titles.push(title);
+        let body = $('.et_pb_text_inner').text();
+        bodies.push(body);
+    }
+
+    const data = mapData(titles, bodies);
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+    
+    // browser.close();
+}
+
+app.get("/news", async (req, res) => {
+   const jsonData = await getArticles();
+   res.send(jsonData);
+})
+
+app.get("/test", (req, res) => {
+    res.send("test");
 })
 
 app.listen(port, () => {
